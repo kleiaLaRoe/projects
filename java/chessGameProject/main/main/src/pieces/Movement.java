8676526e2f08c37d74a76e8b73dfcb89844c86ec
quentonLaroe <quentonlaroe@gmail.com>
2020-05-25 12:35:03 -0500

    Update classes for JUnit tests
    Noticed classes were not working as expected. Updated for JUnit testing.

--- java/chessGameProject/main/main/src/pieces/Movement.java
+++ java/chessGameProject/main/main/src/pieces/Movement.java
@@ -6,39 +6,49 @@ import main.src.board.Coordinate;
 public class Movement {
 
     private Board board;
-    public Movement(){}
+    public Movement(Board gameBoard){
+        setBoard(gameBoard);
+    }
+
+    public Movement(){};
+
+    private void setBoard(Board gameBoard) { board = gameBoard; }
+    public void updateBoard(Board gameBoard) { setBoard(gameBoard); }
 
     public boolean validMovement(Piece piece)
     {
-        if(piece != null)
-        switch(piece.getPieceTag()) {
-            case KING:
-                return distance(piece.getPosition(), piece.getPrevPosition()) == 1;
-            case QUEEN:
-                return slope(piece.getPosition(), piece.getPrevPosition()) == 1
-                        || slope(piece.getPosition(), piece.getPrevPosition()) == 0;
-            case BISHOP:
-                return slope(piece.getPosition(), piece.getPrevPosition()) == 1;
-            case KNIGHT:
-                return (slope(piece.getPosition(), piece.getPrevPosition()) == 2
-                        || slope(piece.getPosition(), piece.getPrevPosition()) == 0.5)
-                        && distance(piece.getPosition(), piece.getPrevPosition()) == 1.5;
-            case ROOK:
-                return slope(piece.getPosition(), piece.getPrevPosition()) == 0;
-            case PAWN:
-                return distance(piece.getPosition(), piece.getPrevPosition()) == 1 ||
-                        slope(piece.getPosition(), piece.getPrevPosition()) == 1
-                                && board.isOccupiedByOpponent(piece.getPlayer(), piece.getPosition())
-                                && piece.isMovingForward();
-            default:
-                return false;
+        if(piece != null) {
+            switch (piece.getPieceTag()) {
+                case KING:
+                    return distance(piece.getPosition(), piece.getPrevPosition()) == 1;
+                case QUEEN:
+                    return slope(piece.getPosition(), piece.getPrevPosition()) == 1
+                            || slope(piece.getPosition(), piece.getPrevPosition()) == 0;
+                case BISHOP:
+                    return slope(piece.getPosition(), piece.getPrevPosition()) == 1;
+                case KNIGHT:
+                    return (slope(piece.getPosition(), piece.getPrevPosition()) == 2
+                            || slope(piece.getPosition(), piece.getPrevPosition()) == 0.5)
+                            && distance(piece.getPosition(), piece.getPrevPosition()) == 1.5;
+                case ROOK:
+                    return slope(piece.getPosition(), piece.getPrevPosition()) == 0;
+                case PAWN:
+                    return distance(piece.getPosition(), piece.getPrevPosition()) == 1 ||
+                            slope(piece.getPosition(), piece.getPrevPosition()) == 1
+                                    && board.isOccupiedByOpponent(piece.getPlayer(), piece.getPosition())
+                                    && piece.isMovingForward();
+                default:
+                    return false;
+            }
         }
         return false;
     }
 
     private double distance(Coordinate current, Coordinate prev)
     {
-        return 0.5 * (Math.abs(current.getX() - prev.getX()) + Math.abs(current.getY() - prev.getY()));
+        if(current.getY() != prev.getY() && current.getX() != prev.getX())
+            return 0.5 * (Math.abs(current.getX() - prev.getX()) + Math.abs(current.getY() - prev.getY()));
+        return (Math.abs(current.getX() - prev.getX()) + Math.abs(current.getY() - prev.getY()));
     }
 
     private double slope(Coordinate current, Coordinate prev)
