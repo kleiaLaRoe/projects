e6ac9b75ffe6cda2ed1a0c0c92258aeefd336c72
quentonLaroe <quentonlaroe@gmail.com>
2021-01-21 22:46:49 -0600

    Create main trait enum
    Create an enumeration that will handle what a character or monster will
    see at time of creation.

---
+++ java/RolePlayingHelper/RolePlayingHelper/src/main/java/com/helper/pathfinder/second/enums/Trait.java
@@ -0,0 +1,43 @@
+package com.helper.pathfinder.second.enums;
+
+public enum Trait {
+    Aberration("Aberration"),
+    Aeon("Aeon"),
+    Air("Air"),
+    Angel("Angel"),
+    Animal("Animal"),
+    Archon("Archon"),
+    Astral("Astral"),
+    Azata("Azata"),
+    Beast("Beast"),
+    Celestial("Celestial"),
+    Cold("Cold"),
+    Construct("Construct"),
+    Daemon("Daemon"),
+    Demon("Demon"),
+    Devil("Devil"),
+    Dragon("Dragon"),
+    Earth("Earth"),
+    Elemental("Elemental"),
+    Ethereal("Ethereal"),
+    Fey("Fey"),
+    Fiend("Fiend"),
+    Fire("Fire"),
+    Fungus("Fungus"),
+    Giant("Giant"),
+    Humanoid("Humanoid"),
+    Inevitable("Inevitable"),
+    Monitor("Monitor"),
+    Ooze("Ooze"),
+    Plant("Plant"),
+    Protean("Protean"),
+    Rakashasa("Rakashasa"),
+    Spirit("Spirit"),
+    Swarm("Swarm"),
+    Undead("Undead"),
+    Water("Water");
+
+    private String label;
+
+    Trait(String label) { this.label = label; }
+}
